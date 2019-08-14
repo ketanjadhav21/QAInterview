f7bc2bc352e2be8c04ed95c4372d7fa13abcdb6d
ketanjadhav21 <jketan21@gmail.com>
2019-08-13 22:06:38 -0700

    Update NBCTest.java
    Added examples for POST, PUT & DELETE

--- Assignment/src/test/java/AssignmentNBC/tests/NBCTest.java
+++ Assignment/src/test/java/AssignmentNBC/tests/NBCTest.java
@@ -12,7 +12,8 @@ import com.jayway.restassured.path.json.*;
 
 public class NBCTest  {
 	
-	
+	final static String ROOT_URI = "http://localhost:7000/employees";
+
 	@Test(description="This test verifies that when the limit parameter is set to 10 then the response returns only 10 counts")
 	public void testNBC_limit10(){
 		int maxlimitval = 10;
@@ -175,7 +176,48 @@ JsonPath js = new JsonPath(res.asString());
 	}
 	
 	
+	//POST Call
+	@Test
+	public void post_test() {
+		Response response = given().
+				contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.body("{\"name\": \"Lisa\",\"salary\": \"2000\"}")
+				.when()
+				.post(ROOT_URI + "/create");
+		System.out.println("POST Response\n" + response.asString());
+		// tests
+		response.then().body("id", Matchers.any(Integer.class));
+		response.then().body("name", Matchers.is("Lisa"));
+	}
+
+
+	//PUT Call
+	@Test
+	public void put_test() {
+		Response response = given()
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.body("{\"name\": \"Lisa Tamaki\",\"salary\": \"20000\"}")
+				.when()
+				.put(ROOT_URI + "/update/3");
+		System.out.println("PUT Response\n" + response.asString());
+		// tests
+		response.then().body("id", Matchers.is(3));
+		response.then().body("name", Matchers.is("Lisa Tamaki"));
+		response.then().body("salary", Matchers.is("20000"));
+	}
 	
-	
+	//DELETE Call
+	@Test
+	public void delete_test() {
+		Response response = delete(ROOT_URI + "/delete/3");
+		System.out.println(response.asString());
+		System.out.println(response.getStatusCode());
+		// check if id=3 is deleted
+		response = get(ROOT_URI + "/list");
+		System.out.println(response.asString());
+		response.then().body("id", Matchers.not(3));
+	}
 
 }
